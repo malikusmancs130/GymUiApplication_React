23c61838db74d63137da4939a7c068876585c45f
Malik Usman <usmancs130@gmail.com>
2023-04-10 22:36:45 +0200

    Add Contact us Page with email.

--- GymApplication/src/App.tsx
+++ GymApplication/src/App.tsx
@@ -3,6 +3,8 @@ import Navbar from "./scenes/navbar";
 import Home from "./scenes/home";
 import Benefits from "./scenes/benefits";
 import { SelectedPage } from "./shared/types"
+import OurClasses from "./scenes/ourClasses";
+import ContactUs from "./scenes/contactUs";
 
 
 function App() {
@@ -31,6 +33,8 @@ function App() {
     />
     <Home  setSelectedPage={setSelectedPage} />
     <Benefits setSelectedPage={setSelectedPage} />
+    <OurClasses setSelectedPage={setSelectedPage} />
+    <ContactUs setSelectedPage={setSelectedPage} />
   </div>;
 }
 
